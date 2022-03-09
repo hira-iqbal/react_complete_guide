1e624cce0fc9d28b3f154af2e06d61710bf6af92
Hira Iqbal <hirach502@gmail.com>
2022-03-09 14:03:05 +0500

    adding two way binding

--- src/components/newexpense/ExpenseForm.js
+++ src/components/newexpense/ExpenseForm.js
@@ -37,7 +37,10 @@ const ExpenseForm = ()=> {
       date: new Date(eneteredDate)
     };
 
-    console.log(event.target.value);
+    console.log(ExpenseData);
+    setEneteredTitle('');
+    setEneteredAmount('');
+    setEneteredDate('');
   };
 
   return (
@@ -45,17 +48,17 @@ const ExpenseForm = ()=> {
       <div className="new-expense__controls ">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange = {TitleChangeHandler} />
+          <input type="text" value = {eneteredTitle} onChange = {TitleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange = {AmountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" value = {eneteredAmount} onChange = {AmountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange = {DateChangeHandler}/>
+          <input type="date" min="2019-01-01" max="2022-12-31" value = {eneteredDate} onChange = {DateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
